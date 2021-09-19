69aaabc88315f9bddfee24c389b1f47342e0e3fb
Brody <Mondomontage@gmail.com>
2021-09-19 22:09:21 +1000

    changed token structure

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace NetScratcher
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            DotNetEnv.Env.Load();
-            var appID           = Environment.GetEnvironmentVariable("REDDIT_APP_ID");
-            var refreshToken    = Environment.GetEnvironmentVariable("MY_REFRESH_TOKEN");
+        {   
+            //create a secret.json and store your json values like so
+            // "REDDIT_APP_ID" : "appid",
+            // "MY_REFRESH_TOKEN" : "refreshtoken"
+            string secret = ("secret.json");
+            JObject SObj = JObject.Parse(File.ReadAllText(secret));
+            string appID = (string)SObj["REDDIT_APP_ID"];
+            string refreshToken = (string)SObj["MY_REFRESH_TOKEN"];
+
             Search search = new Search();
 
             string sub ="";
@@ -17,10 +24,10 @@ namespace NetScratcher
             int postLimit = 0;
 
             try{
-                sub = args[0];
-                key = args[1];
-                sort = args[2]; //relevence hot top new comments
-                postLimit = int.Parse(args[3]);
+                sub         = args[0];
+                key         = args[1];
+                sort        = args[2]; //relevence hot top new comments
+                postLimit   = int.Parse(args[3]);
                     
             }
             catch{
